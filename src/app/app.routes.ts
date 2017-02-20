a5f6aac24f11bdf07074f2df4f344a7bf3076afa
Saideep Chhetri <saideep_chhetri@yahoo.com>
2017-02-20 16:44:46 +0000

    animations in angular2

---
+++ src/app/app.routes.ts
@@ -0,0 +1,15 @@
+
+import { ModuleWithProviders } from "@angular/core";
+import { Routes, RouterModule} from "@angular/router";
+
+import { AppComponent } from './app.component';
+import { HowtoComponent } from './howto/howto.component';
+import { ExamplesComponent } from './examples/examples.component';
+
+export const appRoutes: any = [
+    { path: '', redirectTo: 'howto' , pathMatch: 'full' },
+    { path: 'howto', component: HowtoComponent },
+    { path: 'examples', component: ExamplesComponent }
+];
+
+export const routes: ModuleWithProviders = RouterModule.forRoot(appRoutes);
